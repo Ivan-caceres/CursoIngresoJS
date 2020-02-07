5c7770830956435cfc5ed3cb8094d3e4bcab5330
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-07 12:24:00 -0300

    tres de parcial
    ejercicio de sacar descuento

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/tres.js
@@ -1,4 +1,18 @@
 function mostrar()
 {
+	var ingreso;
+	var porcentaje;
+	var precioFinal;
+	var descuento;
 
+	ingreso=prompt("Ingrese un numero");
+	ingreso=parseInt(ingreso);
+
+	porcentaje=prompt("Ingrese el porcentaje a calcular");
+	porcentaje=parseInt(porcentaje);
+
+	descuento=ingreso*porcentaje/100;
+	
+	precioFinal=document.getElementById('elPrecioFinal').value=descuento+ingreso;
+	console.log(precioFinal);
 }
