79c5f5412a26d74561689ab550c42645c6b86b18
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-07 10:18:06 -0300

    Ejercicio 3 Segunda clase
    Tomamos datos por Id con document.getElementById().
    y los mostramos por alert.

--- 1-EntradaSalida/jsEntradaSalida-3.js
+++ 1-EntradaSalida/jsEntradaSalida-3.js
@@ -2,7 +2,15 @@
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
-	
+	var nombre;
+    
+	nombre=document.getElementById('elNombre').value;
+    
+    //el archivo js esta conectado con el archivo html//
+	//el Id nos sirve para tomar datos de los cuadros de texto//
+	//.value se usa para tomar el valor que ingresamos en el cuadro de texto//
+
+	alert(nombre);
 }
 
 
