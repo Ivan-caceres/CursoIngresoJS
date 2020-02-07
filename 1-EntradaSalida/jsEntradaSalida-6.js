87a0f3441f77c3d0f94c9a8ef2b01f151b8c9df1
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-07 10:19:34 -0300

    Ejercicio 6 Segunda clase

--- 1-EntradaSalida/jsEntradaSalida-6.js
+++ 1-EntradaSalida/jsEntradaSalida-6.js
@@ -3,6 +3,25 @@ transformarlos a enteros (parseInt) y Sumarlos.
 mostrar el resulto por medio de "ALERT"*/
 function sumar()
 {
+	var primerNumero;
+	var segundoNumero;
+	var suma;
 
+	primerNumero=document.getElementById('numeroUno').value;
+	console.log(primerNumero);
+	/*si dejamos el codigo de esta manera
+	 nos va a reconocer el valor en forma de palabra y no de numero
+	para "transformalo" en numero utilizamos el codigo parseInt()*/
+	primerNumero=parseInt(primerNumero);
+
+	segundoNumero=document.getElementById('numeroDos').value;
+	console.log(segundoNumero);
+	
+	segundoNumero=parseInt(segundoNumero);
+
+	suma= (primerNumero+segundoNumero);
+	//los () no son necesarios//
+
+	alert(suma);
 }
 
