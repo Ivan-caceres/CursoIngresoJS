99ca2f23d72436d429207ee1f7464cd814c2835f
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-08 15:55:34 -0300

    Ferrete de construccion
    averiguar la forma de poner el numero pi como variable

--- 8-TPs/jsFerreteConstruccion.js
+++ 8-TPs/jsFerreteConstruccion.js
@@ -6,13 +6,66 @@ C.	Para hacer un contrapiso de 1m x 1m se necesitan 2 bolsas de cemento y 3 de c
 
 function Rectangulo () 
 {
+	var largoDelTerreno;
+	var anchoDelTerreno;
+	var terrenoRectangular;
+	var hilosDeAlambre;
+	var cantidadDeAlambre;
+
+	largoDelTerreno=document.getElementById('Largo').value;
+	largoDelTerreno=parseInt(largoDelTerreno);
+
+	anchoDelTerreno=document.getElementById('Ancho').value;
+	anchoDelTerreno=parseInt(anchoDelTerreno);
+
+	terrenoRectangular=2*(largoDelTerreno+anchoDelTerreno);
+	
+	hilosDeAlambre=3;
+
+	cantidadDeAlambre=terrenoRectangular*hilosDeAlambre;
+
+	alert("La cantidad de alambre a comprar es de "+cantidadDeAlambre+" metros");
 
 }
 function Circulo () 
 {
+	var radioDelTerreno;
+	var terrenoCircular;
+	var hilosDeAlambre;
+	var cantidadDeAlambre;
 	
+	radioDelTerreno=document.getElementById('Radio').value;
+	radioDelTerreno=parseInt(radioDelTerreno);
+
+	terrenoCircular=2*3.14*radioDelTerreno; //averiguar como poner el numero pi como una variable//
+
+	hilosDeAlambre=3;
+
+	cantidadDeAlambre=terrenoCircular*hilosDeAlambre;
+
+	alert("La cantidad de alambre a comprar es de "+cantidadDeAlambre+" metros");
 }
 function Materiales () 
 {
-	
-}
+	var largoDelTerreno;
+	var anchoDelTerreno;
+	var terrenoRectangular;
+	var bolsasDeCemento;
+	var bolsasDeCal;
+
+	largoDelTerreno=document.getElementById('Largo').value;
+	largoDelTerreno=parseInt(largoDelTerreno);
+
+	anchoDelTerreno=document.getElementById('Ancho').value;
+	anchoDelTerreno=parseInt(anchoDelTerreno);
+
+	terrenoRectangular=largoDelTerreno*anchoDelTerreno;
+
+	bolsasDeCemento=terrenoRectangular*2;
+
+	bolsasDeCal=terrenoRectangular*3;
+
+	alert("Se necesitan "+bolsasDeCemento+" bolsas de cemento y "+bolsasDeCal+" bolsas de cal");
+
+
+}	
