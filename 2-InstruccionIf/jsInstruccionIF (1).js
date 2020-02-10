b55f13c15a265c6809a2ed6652c58723354b4d6f
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-10 03:38:02 -0300

    Instruccion If 1
    Al usar if debemos establecer la condicion entre parentecis y utilizar llaves adicionales

--- 2-InstruccionIf/jsInstruccionIF (1).js
+++ 2-InstruccionIf/jsInstruccionIF (1).js
@@ -1,7 +1,13 @@
 function mostrar()
-{
-//tomo la edad  
+ {
+	//tomo la edad
+	var edad;
+	edad=document.getElementById('edad').value;
+	//al usar if debemos usar las llaves//
+	if (edad==15) {
+		alert("Niña bonita");
+	}
 
 
 
-}//FIN DE LA FUNCIÓN
+}//FIN DE LA FUNCION
