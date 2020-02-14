816a885ef4ee665ba72bdce08dfc4cb310daf605
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-14 06:35:57 -0300

    Ejercicios de If

--- 2-InstruccionIf/jsInstruccionIF (10).js
+++ 2-InstruccionIf/jsInstruccionIF (10).js
@@ -1,6 +1,17 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
-	
+	var notas;
+
+	notas=Math.floor(Math.random() *11);
+	notas=parseInt(notas);
+
+	if (notas<4) {
+		alert("Vamos, la proxima se puede");
+	}else if (notas>=4&&notas<9) {
+		alert("APROBO");
+	}else {
+		alert("EXELENTE");
+	}	
 
 }//FIN DE LA FUNCIÓN
--- 2-InstruccionIf/jsInstruccionIF (9).js
+++ 2-InstruccionIf/jsInstruccionIF (9).js
@@ -1,6 +1,10 @@
 function mostrar()
 {
 	//Genero el número RANDOM entre 1 y 10 
+	var random;
 	
+	random=Math.floor(Math.random() *11);
+
+	alert(random);
 
 }//FIN DE LA FUNCIÓN
