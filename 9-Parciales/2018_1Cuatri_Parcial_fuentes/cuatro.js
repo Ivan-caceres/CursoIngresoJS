84aed23cab12594cf80030b419a0a086fe9ac54a
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-14 01:24:32 -0300

    REVISAR

--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/cuatro.js
@@ -1,4 +1,28 @@
 function mostrar()
 {
+	var numeroUno;
+	var numeroDos;
+	var resta;
+	var suma;
 
+	numeroUno=prompt("Ingrese el primer numero");
+	numeroUno=parseInt(numeroUno);
+
+	numeroDos=prompt("Ingrese el segundo numero");
+	numeroDos=parseInt(numeroDos);
+
+	resta=numeroUno-numeroDos;
+
+	suma=numeroUno+numeroDos;
+
+	if (numeroUno>numeroDos) {
+		alert(resta);
+	}else if (suma>10) {
+		alert("La suma es "+suma+" y supera el 10");
+	}else if(numeroUno<numeroDos) {
+		alert(suma);
+	}else {
+		alert(suma)
+	}
 }
+
