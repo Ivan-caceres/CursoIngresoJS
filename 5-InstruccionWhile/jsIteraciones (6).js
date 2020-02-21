9c0c689b828088c3e5a89127b421c1978869feaf
Ivan <60740229+Ivan-caceres@users.noreply.github.com>
2020-02-21 12:21:30 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteraciones (6).js
+++ 5-InstruccionWhile/jsIteraciones (6).js
@@ -1,12 +1,32 @@
 function mostrar()
 {
 
-	var contador=0;
-	var acumulador=0;
+	var contador; 	
+	var acumulador;
+	var numeroIngresado; 
+
+	contador=0;
+	acumulador=0;
+
+	while (contador<5) 
+	{		
+		contador=contador+1;
+
+		numeroIngresado=prompt("Ingrese un numero");
+		numeroIngresado=parseInt(numeroIngresado);
+		while (isNaN(numeroIngresado)) 
+		{
+			numeroIngresado=prompt("Ingrese un numero");
+			numeroIngresado=parseInt(numeroIngresado);
+		}
+
+		acumulador=acumulador+numeroIngresado;
+	}	
+
 
 
 
 document.getElementById('suma').value=acumulador;
-document.getElementById('promedio').value=acumulador/5;
+document.getElementById('promedio').value=acumulador/contador;
 
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (8).js
+++ 5-InstruccionWhile/jsIteraciones (8).js
@@ -4,8 +4,37 @@ function mostrar()
 	var contador=0;
 	var positivo=0;
 	var negativo=1;
+	var numeroIngresado;
 	
 	var respuesta='si';
+
+	while (respuesta=="si") 
+		{					
+			contador=contador+1;
+
+			numeroIngresado=prompt("Ingrese un numero");
+			numeroIngresado=parseInt(numeroIngresado);
+			while(isNaN(numeroIngresado)) 
+			{
+				numeroIngresado=prompt("Ingrese un numero");
+				numeroIngresado=parseInt(numeroIngresado);
+			}
+
+			if (numeroIngresado>0) 
+			{
+				positivo=numeroIngresado+positivo;
+			}
+			else
+			{
+				if (numeroIngresado<0) 
+				{
+					negativo=numeroIngresado*negativo;
+				}
+
+			}
+			
+			respuesta=prompt("Ingrese si para continuar");
+		}	
 
 
 document.getElementById('suma').value=positivo;
